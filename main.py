59fb184bedd6529b52456485c438f1d16713a405
Goldenprince8420 <rahulgolder0202@gmail.com>
2022-06-13 14:37:54 +0530

    first commit

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-# This is a sample Python script.
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import seaborn as sns
+import warnings
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from scipy.signal import butter, sosfilt, sosfilt_zi, sosfiltfilt, lfilter, lfilter_zi, filtfilt, sosfreqz, resample
+# from utils import hamilton_detector, christov_detector, findpeaks, engzee_detector
+# from ecg_detectors.ecgdetectors import Detectors, MWA, panPeakDetect, searchBack
+
+np.random.seed(256)
+sns.set()
+
+warnings.filterwarnings('ignore')
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+if __name__ == "__main__":
+    data = None
+    fs = 200
+    lowcut = 0.05 * 3.3  # 9.9 beats per minute
+    highcut = 15  # 900 beats per minute
+    
+
+# Sample rate and Desired Cutoff Frequencies
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
